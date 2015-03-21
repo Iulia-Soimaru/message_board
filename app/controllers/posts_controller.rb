b77096fbb5b13d0d2fb7425a77f226f20fb90a95
Iulia Soimaru <yuliga4@gmail.com>
2015-03-21 16:10:27 -0700

    create new post link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(title: params[:post][:title], content: params[:post][:content])
+    User.first.posts << @post
+    redirect_to @post
+  end
+
 end
